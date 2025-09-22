3014bf5dd59e7bfcdcd95c9457ccd71607539f2b
Jon Johnson <your_email@example.com>
2025-09-21 20:38:29 -0500

    Add "Please" to print statement in validate.py

--- Validate-Email/validate.py
+++ Validate-Email/validate.py
@@ -6,7 +6,7 @@
 import re
 
 email_conditions = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-user_email = input("Enter your email: ")
+user_email = input("Please Enter your email: ")
 
 isValid = re.search(email_conditions, user_email)
 
