52cc1172205bb27803219db430fdc32c862689af
Jon Johnson <your_email@example.com>
2025-09-21 20:36:36 -0500

    Made E capital in print statement in URL Sortener

--- URL Sortener/main.py
+++ URL Sortener/main.py
@@ -5,5 +5,5 @@ def shorten_url(url):
     return pyshorteners.Shortener().tinyurl.short(url)
 
 
-url = input("Please enter URL: ")
+url = input("Please Enter URL: ")
 print("URL after Shortening : ", shorten_url(url))
